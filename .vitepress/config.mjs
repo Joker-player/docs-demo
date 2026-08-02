757b0317110f2780a962e2fc98535366ace9c010
Joker-player <965330677@qq.com>
2026-08-02 16:45:45 +0800

    修正base路径，CI自动生成.nojekyll修复样式丢失

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,7 @@ import { set_sidebar } from "./utils/auto_sidebar.mjs";	// 改成自己的路径
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/docs-demo/',
   head: [["link", { rel: "icon", href: "/yogo.png" }]],
   title: "吃西瓜的小a",
   description: "A VitePress Site",
